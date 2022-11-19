a9e32f65513f3493661c9124acbda1f71d7b5884
TrungNguyen1409 <trungnguyenb0k30@gmail.com>
2022-11-19 01:14:07 +0100

    custom template

---
+++ src/index.js
@@ -0,0 +1,26 @@
+//@ts-check
+
+/**
+ * Student Name
+ * @type {string}
+ */
+const studentName = "John Doe";
+
+
+
+/**
+ * Array of grades
+ * @type {Array<number>}
+ */
+const grades = [98,97.7,76,89];
+
+
+/**
+ * Todo object
+ * @type {{id: number|string, text: string}}
+ */
+const todo = {
+    id: 1,
+    text: 'Hello'
+    
+}
