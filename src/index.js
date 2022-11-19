60a6b64aa9b83d96b8b42cf82cf3c47eabaa1d6b
TrungNguyen1409 <trungnguyenb0k30@gmail.com>
2022-11-19 11:38:58 +0100

    linking to other part with "Link"

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@
 const studentName = "John Doe";
 
 
-
 /**
  * Array of grades
  * @type {Array<number>}
@@ -24,3 +23,79 @@ const todo = {
     text: 'Hello'
     
 }
+
+/**
+ * Calculate tax
+ * @param {number} amount - Total amount
+ * @param {number} tax - Tax percentage
+ * @returns {string} - Total with a dollar sign
+ */
+
+const calculateTax = (amount, tax) =>{
+    return `$${amount + tax * amount}`;
+};
+
+console.log(calculateTax(100,0.1));
+
+
+/**
+ * A student 
+ * @typedef {Object} Student
+ * @property {number} id - Student ID 
+ * @property {string} name - Student name 
+ * @property {string|number} [age] - Student age (optional)
+ * @property {boolean} isActive - Student is active 
+ * 
+ */
+
+
+/**
+ * @type {Student}
+ * 
+ */
+const student = {
+    id: 1,
+    name: "Trung",
+    age: 22,
+    isActive: true
+};
+
+/**
+ * Class to create a person object
+ */
+class Person {
+    /**
+     * @param {Object} personInfo Information about the person
+     */
+    constructor(personInfo){
+        /**
+         * @property {string} name Person age
+         */
+        this.name = personInfo.name;
+        /**
+         * @property {number} age Person age
+         */
+        this.age = personInfo.age;
+    }
+
+    /**
+     * @property {Function} greet A greeting with the name and age
+     * @returns void
+     */
+    greet(){
+        console.log(`Hello, my name is ${this.name} and I am  ${this.age}`);
+    }
+}
+
+
+/**
+ * Person one
+ *
+ * See {@Link Person}
+ */
+const person1 = new Person({
+    name: 'John Doe',
+    age: 30
+});
+
+console.log(person1.greet());
---
+++ src/util.js
@@ -0,0 +1,21 @@
+/**
+ * A teacher 
+ * @typedef {Object} Teacher
+ * @property {number} id - Teacher ID 
+ * @property {string} name - Teacher name 
+ * @property {string|number} [age] - Teacher age (optional)
+ * @property {boolean} isActive - Teacher is active 
+ * 
+ */
+
+
+/**
+ * 
+ * See {@Link Teacher}
+ */
+ const teacher = {
+    id: 2,
+    name: "Dan",
+    age: 22,
+    isActive: true
+};
